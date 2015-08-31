6fae84556046c8e0160a4190c61581e4bedecc25
Drew Ellison <dme722@gmail.com>
2015-08-31 00:47:48 -0600

    Updated estimator problem statement

--- Technical/algorithms/test/+ToyProblems/+Estimator/run_test.m
+++ Technical/algorithms/test/+ToyProblems/+Estimator/run_test.m
@@ -6,16 +6,15 @@ poseData = Test.Utils.generate_pose_meas_data('square',dt,numFrames,noiseStruct)
 
 estimator = ToyProblems.Estimator.EstimatorExample();
 
-% Your goal is to create your own version of the file ControllerExample
-% that actually calculates desired velocities (the current one that I made
-% is broken since the calc_desired_velocities function does nothing). Then
-% I want you to plot the desired velocities as a function of time in this
+% Your goal is to create your own version of the file EstimatorExample
+% that actually calculates estimated velocities (the current one that I made
+% is broken since the calc_estimated_velocities function does nothing). Then
+% I want you to plot the estimated velocities as a function of time in this
 % function using the data in the variable poseData, which contains x-y
-% positions over time (the code for this already exists below). Start by
-% copying the ControllerExample code into a new script and saving it as
-% ControllerLastName, i.e. Drew's would be ControllerEllison. Then edit
-% anything you see fit to get the controller to work. Once you get it to
-% work, feel free to tune the controller.
+% position measurements over time (the code for this already exists below). Start by
+% copying the EstimatorExample code into a new script and saving it as
+% EstimatorLastName, i.e. Drew's would be EstimatorEllison. Then edit
+% anything you see fit to get the estimator to work.
 
 estimatedVelocity = struct();
 estimatedVelocity.x = [];
