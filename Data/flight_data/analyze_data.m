7c4f18d1fba0e40cd4df0677f0d86b373071b68d
Drew <dme722@gmail.com>
2015-12-06 22:54:23 -0700

    Updates to data analysis and flight control technical document

---
+++ Data/flight_data/analyze_data.m
@@ -0,0 +1,56 @@
+clear all
+close all
+clc
+
+startPercentage = .15;
+endPercentage = .9;
+
+directoryName = '4Dec_unpacked/2015-12-04-19-53-15';
+fileNames = dir(directoryName);
+fileNames = {fileNames(3:end).name};
+
+data = struct();
+
+for fileIdx = 1:numel(fileNames)
+    fieldName = strrep(fileNames{fileIdx}(1:end-4),'-','_');
+    data.(fieldName) = readtable(fullfile(directoryName,fileNames{fileIdx}));
+end
+
+setpointTime = data.mavros_setpoint_position_local.Header_secs- min(data.mavros_setpoint_position_local.Header_secs) + ...
+    data.mavros_setpoint_position_local.Header_nsecs * 1e-9;
+poseTime = data.mavros_local_position_local.Header_secs - min(data.mavros_local_position_local.Header_secs) + ...
+    data.mavros_local_position_local.Header_nsecs * 1e-9;
+
+xsetpoint = data.mavros_setpoint_position_local.Pose_x;
+ysetpoint = data.mavros_setpoint_position_local.Pose_y;
+zsetpoint = data.mavros_setpoint_position_local.Pose_z;
+
+xpose = data.mavros_local_position_local.Pose_x;
+ypose = data.mavros_local_position_local.Pose_y;
+zpose = data.mavros_local_position_local.Pose_z;
+
+startIdxPose = floor(numel(poseTime) * startPercentage) + 1;
+endIdxPose = floor(numel(poseTime) * endPercentage);
+startIdxSetpoint = floor(numel(setpointTime) * startPercentage) + 1;
+endIdxSetpoint = floor(numel(setpointTime) * endPercentage);
+
+figure()
+plot(poseTime(startIdxPose:endIdxPose),xpose(startIdxPose:endIdxPose)), hold on
+plot(setpointTime(startIdxSetpoint:endIdxSetpoint),xsetpoint(startIdxSetpoint:endIdxSetpoint))
+xlabel('Time (s)','FontSize',16)
+ylabel('X Position (m)','FontSize',16)
+grid on
+
+figure()
+plot(poseTime(startIdxPose:endIdxPose),ypose(startIdxPose:endIdxPose)), hold on
+plot(setpointTime(startIdxSetpoint:endIdxSetpoint),ysetpoint(startIdxSetpoint:endIdxSetpoint))
+xlabel('Time (s)','FontSize',16)
+ylabel('Y Position (m)','FontSize',16)
+grid on
+
+figure()
+plot(poseTime(startIdxPose:endIdxPose),zpose(startIdxPose:endIdxPose)), hold on
+plot(setpointTime(startIdxSetpoint:endIdxSetpoint),zsetpoint(startIdxSetpoint:endIdxSetpoint))
+xlabel('Time (s)','FontSize',16)
+ylabel('Z Position (m)','FontSize',16)
+grid on
